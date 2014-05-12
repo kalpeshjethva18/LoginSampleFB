9f28cf73fd807cd8f194b01129018c402e098a63
gabrieltheodoropoulos <gabrielth.devel@gmail.com>
2014-05-12 07:10:27 +0300

    Initial Commit

---
+++ LoginSampleFB/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LoginSampleFB
+//
+//  Created by Gabriel Theodoropoulos on 12/5/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LoginSampleFBTests/LoginSampleFBTests.m
@@ -0,0 +1,34 @@
+//
+//  LoginSampleFBTests.m
+//  LoginSampleFBTests
+//
+//  Created by Gabriel Theodoropoulos on 12/5/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoginSampleFBTests : XCTestCase
+
+@end
+
+@implementation LoginSampleFBTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
