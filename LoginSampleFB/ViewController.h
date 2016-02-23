248bfa9aa000bb796338aad38220b1506e70b7b8
kalpeshjethva <macpc@macpc1-2.local>
2016-02-23 17:53:10 +0530

    loginWithFB
    loginWithFB

--- LoginSampleFB/ViewController.h
+++ LoginSampleFB/ViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <FBLoginViewDelegate>
+
+@property (weak, nonatomic) IBOutlet FBLoginView *loginButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblLoginStatus;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblUsername;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblEmail;
+
+@property (weak, nonatomic) IBOutlet FBProfilePictureView *profilePicture;
 
 @end
--- LoginSampleFB/ViewController.m
+++ LoginSampleFB/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+-(void)toggleHiddenState:(BOOL)shouldHide;
+
 @end
 
 @implementation ViewController
@@ -18,6 +20,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self toggleHiddenState:YES];
+    self.lblLoginStatus.text = @"";
+    
+    self.loginButton.delegate = self;
+    self.loginButton.readPermissions = @[@"public_profile", @"email"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +33,48 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark - Private method implementation
+
+-(void)toggleHiddenState:(BOOL)shouldHide{
+    self.lblUsername.hidden = shouldHide;
+    self.lblEmail.hidden = shouldHide;
+    self.profilePicture.hidden = shouldHide;
+}
+
+
+#pragma mark - FBLoginView Delegate method implementation
+
+-(void)loginViewShowingLoggedInUser:(FBLoginView *)loginView{
+    self.lblLoginStatus.text = @"You are logged in.";
+    
+    [self toggleHiddenState:NO];
+}
+
+
+-(void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user
+{
+    NSLog(@"%@", user);
+    self.profilePicture.profileID = user.id;
+    self.lblUsername.text = user.name;
+    self.lblEmail.text = [user objectForKey:@"email"];
+}
+
+
+-(void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView{
+    
+    
+    [FBSession.activeSession close];
+    [FBSession.activeSession  closeAndClearTokenInformation];
+    FBSession.activeSession=nil;
+    
+    self.lblLoginStatus.text = @"You are logged out";
+    [self toggleHiddenState:YES];
+}
+-(void)loginView:(FBLoginView *)loginView handleError:(NSError *)error{
+    NSLog(@"%@", [error localizedDescription]);
+}
+
 
 @end
